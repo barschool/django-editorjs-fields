5927a15f6daf12235944c87f2acc157edb942242
Peter Larsson <peter@barschool.net>
2024-02-12 02:34:45 +0000

    Changed to setup.py install to enable editable

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name = "django-editorjs-fields",
+    version = "0.2.7",
+    description = "Django plugin for using Editor.js",
+    authors = ["Ilya Kotlyakov <m@2ik.ru>"],
+    license = "MIT",
+    repository = "https://github.com/2ik/django-editorjs-fields",
+    documentation = "https://github.com/2ik/django-editorjs-fields",
+    readme = "README.md",
+    keywords = ["editorjs", "django-editor", "django-wysiwyg", "wysiwyg", "django-admin"],
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
